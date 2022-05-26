0979175d13ae4f0b69786383ea7c4429d8a37a23
jefitzmaurice <jfitzmaurice@cshnyc.org>
2022-05-26 08:41:41 -0400

    add descriptions

--- sketch.js
+++ sketch.js
@@ -5,6 +5,10 @@ let ani = 'Animation Project'
 let vari = 'Variation Project'
 let scr = 'Scroll down to see the projects!'
 
+let pp = 'In the Paint Project, I used If Statements with "mouse is pressed" and "key is pressed" so that the color of the paint brushes could be changed and other actions.'
+let hp = 'In the Happy Face Project, I created a new function called "drawHappyFace" to draw the face, rather than just doing it in the draw function.'
+let ap = 'In the Animation Proejct, I used If Statements and created new functions to draw the heart and sad faces.'
+let vp = 'In the Variation Project, I used For Loops to create the repeating circles.'
 
 let img;
 // function preload() {
@@ -34,6 +38,14 @@ function draw() {
   text(vari, 600, 450, 500, 500);
   fill(235, 64, 52);
   text(scr, 700, 50, 400, 400);
+
+  fill(245, 66, 114);
+  textSize(25);
+  text(pp, 50, 200, 500, 700);
+  text(hp, 50, 500, 500, 700);
+  text(ap, 600, 200, 500, 700);
+  text(vp, 600, 500, 500, 700);
+
 
 
 }
